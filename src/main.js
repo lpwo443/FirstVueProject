c214bbc140088595c24a9b124641fd2e2a4bbaae
lpwo443 <1422885400@qq.com>
2025-03-02 22:02:33 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import {getCategory} from '@/api/testapi'
+getCategory().then(res=>{
+    console.log(res.data)
+})
 const app = createApp(App)
 
 app.use(createPinia())
